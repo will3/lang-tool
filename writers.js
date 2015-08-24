e0b66083c120220d117c358c86a6d706f0815e26
Mikhail Diatchenko <mikhail.diatchenko@vista.co>
2015-08-25 10:40:09 +1200

    Added Android format support

--- writers.js
+++ writers.js
@@ -6,7 +6,7 @@ function TextWriter() {
     console.log('');
     for (var i = 0; i < data.length; i++) {
       console.log(data[i].code + '\t' + data[i].text);
-    };
+    }
     console.log('');
     console.log("% items writted", data.length);
   };
@@ -24,7 +24,43 @@ function JsonWriter() {
   };
 }
 
+var builder = require('xmlbuilder');
+
+function AndroidWriter() {
+  this.write = function(data, note, program) {
+    var root = builder.create('resources');
+    root.comment('Last Update: ' + new Date().toLocaleString());
+    root.comment('By: langtool-node v'+program.version());
+    root.att('language', program.language || 'en');
+    root.att('utcdategenerated', new Date().toISOString());
+    root.att('note', note);
+
+    for (var i = 0; i < data.length; i++) {
+      var entry = data[i];
+      var comment = '';
+      if (entry.comment) {
+        comment = entry.comment;
+      }
+
+      if (entry.note) {
+        if (comment)
+          comment += '.  ';
+        comment = entry.note;
+      }
+
+      root.ele('string', {
+        name: entry.code,
+        comment: comment,
+        section: entry.section
+      }, entry.text);
+    }
+
+    console.log(root.end({ pretty: true}));
+  };
+}
+
 module.exports = {
   text: TextWriter,
-  json: JsonWriter
+  json: JsonWriter,
+  android: AndroidWriter
 }
